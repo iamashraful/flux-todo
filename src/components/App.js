274c7eca890333dba1a404dc2a757fb42b8bea81
Ashraful Islam <ashraful@field.buzz>
2017-06-11 10:07:55 +0600

    Initial Commit for TODO App

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../assets/css/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Welcome to REACT & FLUX</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
