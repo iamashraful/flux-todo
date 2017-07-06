647bb1f7fb9a55b3e0b3fabf5f6383559b0c3c0c
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-07-06 16:19:07 +0600

    Serving tasks/todos from separated components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 
-import TodoStore from "../store/TodoStore";
 import * as TodoActions from "../actions/TodoActions";
 import Todo from "./Todo";
 import InputForm from "./InputForm";
@@ -8,36 +7,11 @@ import Navbar from "./Navbar";
 import "../assets/css/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: TodoStore.getAll(),
-            defaultInputText: ''
-        }
-    }
-
-    componentWillMount() {
-        // This function will fire up when store will change
-        TodoStore.on("change", () => {
-            this.setState({
-                todos: TodoStore.getAll()
-            });
-        });
-    }
-
     getTask(val) {
         TodoActions.createTodo(val);
     }
 
     render() {
-        const {todos} = this.state;  // get the todos state and copy to todos
-        const TodoComponent = todos.map((todo) => {
-            if (todo.isCompleted)
-                return <del><Todo item={todo.text} key={todo.id}/></del>;
-            else
-                return <Todo item={todo.text} key={todo.id}/>
-        });
-
         return (
             <div className="App">
                 <div className="app-nav">
@@ -45,11 +19,6 @@ class App extends Component {
                     <InputForm getTask={this.getTask.bind(this)}/>
                 </div>
                 <Navbar className="todo-navbar"/>
-
-
-                <div className="todo">
-                    {TodoComponent}
-                </div>
             </div>
         );
     }
--- src/components/CompletedTask.js
+++ src/components/CompletedTask.js
@@ -3,12 +3,39 @@
  */
 
 import React, {Component} from 'react';
-
+import Todo from '../components/Todo';
+import TodoStore from "../store/TodoStore";
 
 class CompletedTask extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: TodoStore.getAll(),
+        }
+    }
+
+    componentWillMount() {
+        // This function will fire up when store will change
+        TodoStore.on("change", () => {
+            this.setState({
+                todos: TodoStore.getAll()
+            });
+        });
+    }
+
     render() {
+        const {todos} = this.state;  // get the todos state and copy to todos
+        const TodoComponent = todos.map((todo) => {
+            if (todo.isCompleted)
+                return <del><Todo item={todo.text} key={todo.id}/></del>;
+            else
+                return <Todo item={todo.text} key={todo.id}/>
+        });
+
         return (
-            <p>Completed Todos</p>
+            <div className="todo">
+                {TodoComponent}
+            </div>
         )
     }
 }
