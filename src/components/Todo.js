7f10a616b51d4afa5dfbecf2c00dcc001fd6a512
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-07-17 16:32:39 +0600

    Added minor styling for todo app

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,11 +7,16 @@ import React, {Component} from "react";
 class Todo extends Component {
     render() {
         return (
-            <div>
-                <li>
-                    <input type="checkbox"/>
+            <div className="row list-item">
+                <div className="col-md-2 col-sm-2">
+                    <button className="btn btn-sm btn-outline-success">In Progress</button>
+                </div>
+                <div className="col-md-8 col-sm-8 text-left">
                     {this.props.item}
-                </li>
+                </div>
+                <div className="col-md-2 col-sm-2">
+                    <button className="btn btn-sm btn-outline-danger">Complete</button>
+                </div>
             </div>
         )
     }
